d1d78928d2e864c752f61d127369163d7b922d6e
Shailesh Sharma <sailesh9521@gmail.com>
2026-07-10 00:44:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (11.38%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+    int freq[256] = {0};
+
+    for(char ch:s){
+        freq[ch - 'a']++;
+    }
+
+    for(char ch:t){
+        freq[ch - 'a']--;
+    }
+    
+    for(int i=0; i < 256; i++){
+        if(freq[i] != 0){
+            return false;
+        }
+    }
+    return true;
+}
+};
