a663bb347029260659bb30850155299b8e810e7e
Shailesh Sharma <sailesh9521@gmail.com>
2026-07-10 01:15:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (70.31%) - LeetSync

---
+++ 2406-decode-the-message/decode-the-message.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string decodeMessage(string key , string message){
+    // creating mapping 
+    char start = 'a';
+    char mapping[280] ={0};
+    
+    for(auto ch:key){
+        if(ch != ' ' && mapping[ch] == 0){
+            mapping[ch] = start;
+            start++;
+        }
+    }
+
+    // use mapping
+    string ans;
+    for(int i=0; i<message.length(); i++){
+       
+        char ch = message[i];
+        if(ch == ' '){
+            ans.push_back(' ');
+        }
+        else{
+            char decodedChar = mapping[ch];
+             ans.push_back(decodedChar);
+        }             
+    }
+    return ans;
+}
+};
