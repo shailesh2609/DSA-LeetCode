4a27c36b188ff28820d2ebad190e49a3d4ef641d
Shailesh Sharma <sailesh9521@gmail.com>
2026-07-14 16:04:20 +0530

    Time: 4 ms (66.91%) | Memory: 10.1 MB (86.68%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-   int romanToInt(string s) {
-    unordered_map<char,int>mp ={
-        {'M',1000},
-        {'D',500},
-        {'C',100},
-        {'L',50},
-        {'X',10},
-        {'V',5},
-        {'I',1}
-    };
-
+    int checkRoman(char ch){
+    if(ch == 'M') return 1000;
+    if(ch == 'D') return 500;
+    if(ch == 'C') return 100;
+    if(ch == 'L') return 50;
+    if(ch == 'X') return 10;
+    if(ch == 'V') return 5;
+    return 1;
+}
+int romanToInt(string s) {
     int ans = 0;
-    for(int i=0; i< s.size(); i++){
-        if(i + 1 < s.size() && mp[s[i]] < mp[s[i+1]]){
-            ans = ans - mp[s[i]];
+    for(int i = 0; i < s.size(); i++){
+        if(i+1 < s.size() && checkRoman(s[i]) < checkRoman(s[i+1])){
+            ans -= checkRoman(s[i]);
         }
         else{
-            ans = ans + mp[s[i]];
+            ans += checkRoman(s[i]);
         }
     }
     return ans;
