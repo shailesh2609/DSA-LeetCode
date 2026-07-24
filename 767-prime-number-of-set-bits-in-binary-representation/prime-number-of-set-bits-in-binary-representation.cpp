ad912655feb26a583c734afe58cd46089fc063bd
Shailesh Sharma <sailesh9521@gmail.com>
2026-07-25 03:49:18 +0530

    Time: 31 ms (10.21%) | Memory: 7.9 MB (90.12%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
-bool checkPrime(int num){
-    if(num == 1) return false;
-    int i = 2;
-    while(i < num){
-        if(num % i == 0){
-            return false;
-        }
-        else{
-            i++;
-        }
-    }
-    return true;
-}
-int countSetBits(int left){
-    int bitCount = 0;
-    while(left > 0){
-        if(left % 2 == 1){
-            bitCount++;
-        }
-        left = left / 2;
-    }
-    return bitCount;
-}
-int countPrimeSetBits(int left, int right) {
-    int ans = 0;
-    while(left <= right){
-        if(checkPrime(countSetBits(left))){
-            ans++;
-            left++;
-        }
-        else{
-            left++;
-        }
-    }
-    return ans;
-}
+   bool checkPrime(int left){
+       // Count the Set Bits 
+       int bitCount = 0;
+       while(left > 0){
+           if(left % 2 == 1){
+               bitCount++;
+           }
+           left /= 2;
+       }
+
+       // Check if it is prime or not
+       if(bitCount < 2) return false;
+       for(int i=2; i*i <= bitCount ; i++){
+           if(bitCount % i == 0){
+               return false;
+           }
+       }
+       return true;
+   }
+   int countPrimeSetBits(int left, int right) {
+       int ans = 0;
+       while(left <= right){
+           if(checkPrime(left)){
+               ans++;
+               left++;
+           }
+           else{
+               left++;
+           }
+       }
+       return ans;
+   }
 };
