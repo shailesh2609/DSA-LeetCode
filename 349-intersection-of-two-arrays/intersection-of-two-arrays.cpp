a86d39f0045d5d2d0dbc71e344e858be4931d07a
Shailesh Sharma <sailesh9521@gmail.com>
2026-07-30 20:10:55 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (82.02%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -4,21 +4,22 @@ public:
     vector<int>ans;
     sort(nums1.begin(), nums1.end());
     sort(nums2.begin(), nums2.end());
-    for (int i = 0; i < nums1.size(); i++){
-        if(i > 0 && nums1[i] == nums1[i-1]){
-            continue;
+    int i=0;
+    int j=0;
+    while(i < nums1.size() && j < nums2.size()){
+        if(i> 0 && nums1[i] == nums1[i-1]){
+            i++;
         }
-        for (int j = 0; j < nums2.size(); j++){
-            if(j > 0 && nums2[j] == nums2[j-1]){
-                continue;
-            }
-            else if(nums1[i] == nums2[j]){
-                ans.push_back(nums2[j]);
-                break;
-            }
-            else{
-                continue;
-            }
+        else if(nums1[i] == nums2[j]){
+            ans.push_back(nums1[i]);
+            i++;
+            j++;
+        }
+        else if(nums1[i] < nums2[j]){
+            i++;
+        }
+        else{
+            j++;
         }
     }
     return ans;
